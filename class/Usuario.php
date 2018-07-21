1ec66c1137a6a632f1fec9113620cbddfd3741f9
Tiago Nogueira <tiagonogueirajf@gmail.com>
2018-07-21 17:40:36 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -67,7 +67,7 @@ class Usuario {
 
     public function loadById($id){
 
-    	$sql = new SQL();
+    	$sql = new Sql();
 
     	$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
 
@@ -76,7 +76,7 @@ class Usuario {
 
     	if (count($results) > 0){// verifica se existe
 
-    		$row = results[0];
+    		$row = $results[0];
 
     		$this->setIdusuario($row['idusuario']);
     		$this->setDeslogin($row['deslogin']);
@@ -84,6 +84,23 @@ class Usuario {
     		$this->setDtcadastro(new DateTime($row['dtcadastro']));
 
     	}
+    }
+
+//static pois não precisa instanciar
+    public static function getList(){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+    }
+
+    public static function search($login){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
     }
 
     public function __toString(){
@@ -94,6 +111,34 @@ class Usuario {
     		"dessenha"=>$this->getDessenha(),
     		"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
     	));
+    }
+
+
+    public function login($login, $password){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+
+                ":LOGIN"=>$login,
+                ":PASSWORD"=>$password
+        ));
+
+        if (count($results) > 0){// verifica se existe
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+        } else {
+
+            throw new Exception(" Login ou senha inválidos");
+            
+        }
+
     }
 }
 
--- index.php
+++ index.php
@@ -4,19 +4,49 @@
 require_once("config.php");
 
 
-//$sql = new Sql();
 
-//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+$sql = new Sql();
 
-
-//echo json_encode($usuarios);
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 
+echo json_encode($usuarios);
+
+echo "<br>-----------------------<br>";
+//carrega um usuario
 $root = new Usuario();
 
 
 $root->loadById(2);
 
 echo $root;
+
+echo "<br>--------------------------<br>";
+//chamando metodo estatico
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+
+
+echo "<br>--------------------------<br>";
+
+//carrega uma lista de usuarios buscando pelo login
+
+$search = Usuario::search("te");
+
+echo json_encode($search);
+
+
+echo "<br>--------------------------<br>";
+
+
+//carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("userteste","123456");
+
+echo $usuario;
+
+
 
  ?>
